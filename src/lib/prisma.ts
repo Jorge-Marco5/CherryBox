1f2134c4b76757e783de965e8628dbf6456ec41f
Jorge-Marco5 <luismar1345@gmail.com>
2026-06-24 01:57:20 -0600

    refactor build script in package.json and adjust import order in prisma.ts

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,6 +1,6 @@
 import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "../generated/prisma/client";
 import dotenv from "dotenv";
+import { PrismaClient } from "../generated/prisma/client";
 dotenv.config();
 
 const connectionString = process.env.DATABASE_URL!;
